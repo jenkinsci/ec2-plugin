5787aed9ad63cc2163fcc4eace2fce0c8fc0fec6
kohsuke <kohsuke@dev.java.net>
2008-10-30 00:13:11 +0000

    this is little better

--- src/main/webapp/connectionTest.js
+++ src/main/webapp/connectionTest.js
@@ -1,11 +1,32 @@
+/**
+ * Traverses a form in the reverse document order starting from the given element (but excluding it),
+ * until the given filter matches, or run out of an element.
+ */
+function findPrevious(src,filter) {
+    function prev(e) {
+        var p = e.previousSibling;
+        if(p==null) return e.parentNode;
+        while(p.lastChild!=null)
+            p = p.lastChild;
+        return p;
+    }
+
+    while(src!=null) {
+        src = prev(src);
+        if(filter(src))
+            return src;
+    }
+    return null;
+}
+function findPreviousFormItem(src,name) {
+    return findPrevious(src,function(e){ return e.tagName=="INPUT" && e.name==name; });
+}
+
 function testEC2(rootURL,button) {
   button = button._button;
-  var p = button;
-  p = Element.up(p,"TR");
-  p = Element.previous(p,null,2);
-  pwd = Element.down(p,"INPUT");
-  p = Element.previous(p,null,2);
-  uid = Element.down(p,"INPUT");
+
+  var pwd = findPreviousFormItem(button,"_.secretKey");
+  var uid = findPreviousFormItem(pwd,"_.accessId");
 
   new Ajax.Request(rootURL+"/descriptor/hudson.plugins.ec2.EC2Cloud/testConnection", {
       method: "post",
