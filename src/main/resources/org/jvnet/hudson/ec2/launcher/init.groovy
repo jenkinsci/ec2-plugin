f8a8263d70e389565503080343f1f9a8c30a4420
kohsuke <kohsuke@dev.java.net>
2009-05-24 18:30:24 +0000

    configure the EC2 plugin from the get-go.

--- src/main/resources/org/jvnet/hudson/ec2/launcher/init.groovy
+++ src/main/resources/org/jvnet/hudson/ec2/launcher/init.groovy
@@ -1,16 +1,24 @@
 import hudson.model.*;
 import hudson.security.*;
 
+h = Hudson.getInstance();
+ucl=h.pluginManager.uberClassLoader;
+
 // since EC2 instances are publicly accessible,
 // we need Hudson to start in a password protected manner
-Hudson h = Hudson.getInstance();
 h.setAuthorizationStrategy(new FullControlOnceLoggedInAuthorizationStrategy());
 HudsonPrivateSecurityRealm realm = new HudsonPrivateSecurityRealm(false);
 
 accesskey=new File("/root/accessId.txt").text.trim();
 password =new File("/root/secretKey.txt").text.trim();
+key=new File("/root/ec2.key").text.trim();
+
 realm.createAccount(accesskey,password);
 h.setSecurityRealm(realm);
+
+// configure EC2 plugin
+h.clouds.add(ucl.loadClass("hudson.plugins.ec2.EC2Cloud").newInstance(accesskey,password,key,"",[]));
+
 h.save();
 
 // delete the init file once it's completed
