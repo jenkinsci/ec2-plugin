jenkinsci/ec2-plugin

commit 9af4d7e980accfbdc124345b09a584baf6027f91

Reuse Jec2 connection instead of creating a new one (with a multithreaded HttpClient instance) each time we connect

src/main/java/hudson/plugins/ec2/EC2Cloud.java
@@ -244,9 +244,12 @@ public abstract class EC2Cloud extends Cloud {
     /**
      * Connects to EC2 and returns {@link Jec2}, which can then be used to communicate with EC2.
      */
-    public Jec2 connect() throws EC2Exception {
+    public synchronized Jec2 connect() throws EC2Exception {
         try {
-            return connect(accessId, secretKey, getEc2EndpointUrl());
+            if (connection != null) {
+                connection = connect(accessId, secretKey, getEc2EndpointUrl());
+            }
+            return connection;
         } catch (IOException e) {
             throw new EC2Exception("Failed to retrieve the endpoint",e);
         }
@@ -470,6 +473,7 @@ public abstract class EC2Cloud extends Cloud {
     }
 
     private static final Logger LOGGER = Logger.getLogger(EC2Cloud.class.getName());
+    private Jec2 connection;
 
     private static boolean isSSL(URL endpoint) {
         return endpoint.getProtocol().equals("https");

commit 65e4800f9976bed3d611050082f7c6b595104635

Fixed some bugs introduced by 9af4d7e980accfbdc124345b09a584baf6027f91

src/main/java/hudson/plugins/ec2/EC2Cloud.java
@@ -63,6 +63,8 @@ public abstract class EC2Cloud extends Cloud {
     private final List<SlaveTemplate> templates;
     private transient KeyPairInfo usableKeyPair;
 
+    private transient Jec2 connection;
+    
     protected EC2Cloud(String id, String accessId, String secretKey, String privateKey, String instanceCapStr, List<SlaveTemplate> templates) {
         super(id);
         this.accessId = accessId.trim();
@@ -246,7 +248,7 @@ public abstract class EC2Cloud extends Cloud {
      */
     public synchronized Jec2 connect() throws EC2Exception {
         try {
-            if (connection != null) {
+            if (connection == null) {
                 connection = connect(accessId, secretKey, getEc2EndpointUrl());
             }
             return connection;
@@ -473,7 +475,6 @@ public abstract class EC2Cloud extends Cloud {
     }
 
     private static final Logger LOGGER = Logger.getLogger(EC2Cloud.class.getName());
-    private Jec2 connection;
 
     private static boolean isSSL(URL endpoint) {
         return endpoint.getProtocol().equals("https");