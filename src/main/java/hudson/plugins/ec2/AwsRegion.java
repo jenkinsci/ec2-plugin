c26a52ce583e4b5ea1be9fc52933c52c5e5e3dd1
Frank Bille <github@frankbille.dk>
2011-11-23 22:10:45 +0100

    Fixed wrong translation for Asia Pacific (Tokyo) Region. Also added the new us-west-2, US West (Oregon) Region.

--- src/main/java/hudson/plugins/ec2/AwsRegion.java
+++ src/main/java/hudson/plugins/ec2/AwsRegion.java
@@ -18,9 +18,10 @@ import java.util.Locale;
 public enum AwsRegion {
     US_EAST_1(Messages._AwsRegion_UsEast()),
     US_WEST_1(Messages._AwsRegion_UsWest()),
+    US_WEST_2(Messages._AwsRegion_UsWest2()),
     EU_WEST_1(Messages._AwsRegion_EuWest()),
     AP_SOUTHEAST_1(Messages._AwsRegion_ApSouthEast()),
-    AP_NORTHEAST_1(Messages._AwsRegion_ApSouthEast());
+    AP_NORTHEAST_1(Messages._AwsRegion_ApNorthEast());
 
     public final URL ec2Endpoint,s3Endpoint;
 
