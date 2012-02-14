0e92296878e7d584eafd70a417acc7c727d83ed4
Francis Upton IV <francisu@ieee.org>
2012-02-14 14:33:12 -0800

    Remove conflict markers in previous commit

--- src/main/java/hudson/plugins/ec2/PluginImpl.java
+++ src/main/java/hudson/plugins/ec2/PluginImpl.java
@@ -15,11 +15,8 @@ public class PluginImpl extends Plugin implements Describable<PluginImpl> {
     public void start() throws Exception {
         // backward compatibility with the legacy class name
         Hudson.XSTREAM.alias("hudson.plugins.ec2.EC2Cloud",AmazonEC2Cloud.class);
-<<<<<<< HEAD
-=======
         // backward compatibility with the legacy instance type
         Hudson.XSTREAM.registerConverter(new InstanceTypeConverter());
->>>>>>> 936b1088a15d7ec2bd636f6307f11660f66b7634
 
         // make sure the converter gets registered by forcing static initializer
         AwsRegion.AP_NORTHEAST_1.toString();
