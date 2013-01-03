803dfea25551d92453e99fd39e7e366d41007592
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-01-03 15:00:20 -0500

    Change primary template identification to be its name, not its AMI.
    Many useful configurations will have multiple slave templates that use
    the same AMI (but differ in other aspects). This patch changes various
    parts of the code to properly identify templates by their names (in the
    'description' field) instead of their AMI. As an example, the manual slave
    provisioning button, before this patch, would allow the user to select a
    template from a drop-down list, but then launched the slave using the
    template's AMI, which could have resulted in the wrong template being
    used for the launch. This patch also changes the global config page so
    that the 'description' field is the first field in each template
    definition, to emphasize its importance.
    
    Addresses [JENKINS-7960] and [JENKINS-15158].

--- src/test/java/hudson/plugins/ec2/SlaveTemplateTest.java
+++ src/test/java/hudson/plugins/ec2/SlaveTemplateTest.java
@@ -48,6 +48,7 @@ public class SlaveTemplateTest extends HudsonTestCase {
 
     public void testConfigRoundtrip() throws Exception {
         String ami = "ami1";
+	String description = "foo ami";
 
         EC2Tag tag1 = new EC2Tag( "name1", "value1" );
         EC2Tag tag2 = new EC2Tag( "name2", "value2" );
@@ -55,7 +56,7 @@ public class SlaveTemplateTest extends HudsonTestCase {
         tags.add( tag1 );
         tags.add( tag2 );
 
-        SlaveTemplate orig = new SlaveTemplate(ami, EC2Slave.TEST_ZONE, "default", "foo", "22", InstanceType.M1Large, "ttt", Node.Mode.NORMAL, "foo ami", "bar", "aaa", "10", "rrr", "fff", "-Xmx1g", false, "subnet 456", tags, null, false, null);
+	SlaveTemplate orig = new SlaveTemplate(ami, EC2Slave.TEST_ZONE, "default", "foo", "22", InstanceType.M1Large, "ttt", Node.Mode.NORMAL, description, "bar", "aaa", "10", "rrr", "fff", "-Xmx1g", false, "subnet 456", tags, null, false, null);
 
         List<SlaveTemplate> templates = new ArrayList<SlaveTemplate>();
         templates.add(orig);
@@ -64,12 +65,13 @@ public class SlaveTemplateTest extends HudsonTestCase {
         hudson.clouds.add(ac);
 
         submit(createWebClient().goTo("configure").getFormByName("config"));
-        SlaveTemplate received = ((EC2Cloud)hudson.clouds.iterator().next()).getTemplate(ami);
+        SlaveTemplate received = ((EC2Cloud)hudson.clouds.iterator().next()).getTemplate(description);
         assertEqualBeans(orig, received, "ami,zone,description,remoteFS,type,jvmopts,stopOnTerminate,securityGroups,subnetId,usePrivateDnsName");
     }
 
     public void testConfigRoundtripWithPrivateDns() throws Exception {
         String ami = "ami1";
+	String description = "foo ami";
 
         EC2Tag tag1 = new EC2Tag( "name1", "value1" );
         EC2Tag tag2 = new EC2Tag( "name2", "value2" );
@@ -77,7 +79,7 @@ public class SlaveTemplateTest extends HudsonTestCase {
         tags.add( tag1 );
         tags.add( tag2 );
 
-        SlaveTemplate orig = new SlaveTemplate(ami, EC2Slave.TEST_ZONE, "default", "foo", "22", InstanceType.M1Large, "ttt", Node.Mode.NORMAL,"foo ami", "bar", "aaa", "10", "rrr", "fff", "-Xmx1g", false, "subnet 456", tags, null, true, null);
+        SlaveTemplate orig = new SlaveTemplate(ami, EC2Slave.TEST_ZONE, "default", "foo", "22", InstanceType.M1Large, "ttt", Node.Mode.NORMAL, description, "bar", "aaa", "10", "rrr", "fff", "-Xmx1g", false, "subnet 456", tags, null, true, null);
 
         List<SlaveTemplate> templates = new ArrayList<SlaveTemplate>();
         templates.add(orig);
@@ -86,7 +88,7 @@ public class SlaveTemplateTest extends HudsonTestCase {
         hudson.clouds.add(ac);
 
         submit(createWebClient().goTo("configure").getFormByName("config"));
-        SlaveTemplate received = ((EC2Cloud)hudson.clouds.iterator().next()).getTemplate(ami);
+        SlaveTemplate received = ((EC2Cloud)hudson.clouds.iterator().next()).getTemplate(description);
         assertEqualBeans(orig, received, "ami,zone,description,remoteFS,type,jvmopts,stopOnTerminate,securityGroups,subnetId,tags,usePrivateDnsName");
     }
 }
