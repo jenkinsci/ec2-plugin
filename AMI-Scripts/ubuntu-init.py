e38909cceb7b2ad96b90171aa1ec6999eb8a953d
Adam I <runnirr@gmail.com>
2013-01-31 15:58:07 -0500

    Fix up python script for ubuntu ami
    Fix java param when registering slave
    Add check to see if a version of java is already installed

--- AMI-Scripts/ubuntu-init.py
+++ AMI-Scripts/ubuntu-init.py
@@ -3,8 +3,13 @@ import os
 import httplib
 import string
 
-os.system("sudo apt-get update")
-os.system("sudo apt-get install openjdk-7-jre -y")
+# If java is installed it will be zero
+# If java is not installed it will be non-zero
+hasJava = os.system("java -version")
+
+if hasJava != 0:
+    os.system("sudo apt-get update")
+    os.system("sudo apt-get install openjdk-7-jre -y")
 
 conn = httplib.HTTPConnection("169.254.169.254")
 conn.request("GET", "/latest/user-data")
@@ -16,10 +21,11 @@ jenkinsUrl = ""
 slaveName = ""
 
 for arg in args:
-	if arg.split("=")[0] == "JENKINS_URL":
-		jenkinsUrl = arg.split("=")[1]
-	if arg.split("=")[0] == "SLAVE_NAME":
-		slaveName = arg.split("=")[1]
+    if arg.split("=")[0] == "JENKINS_URL":
+        jenkinsUrl = arg.split("=")[1]
+    if arg.split("=")[0] == "SLAVE_NAME":
+        slaveName = arg.split("=")[1]
 		
 os.system("wget " + jenkinsUrl + "jnlpJars/slave.jar -O slave.jar")
-os.system("java -jar slave.jar -jnlp " + jenkinsUrl + "computer/" + slaveName + "/slave-agent.jnlp")
+os.system("java -jar slave.jar -jnlpUrl " + jenkinsUrl + "computer/" + slaveName + "/slave-agent.jnlp")
+
