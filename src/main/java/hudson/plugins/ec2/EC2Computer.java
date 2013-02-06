9d8a23a41d21aafb7dc6a7d97848aa1f32422c4a
Cory <cms5822@rit.edu>
2013-02-06 15:26:20 -0500

    Slave Config Page Update
    The configuration for a slave configuration page will now show if the
    instance is an on demand, or spot instance. This is done by checking if
    the slave passed into EC2 computer is of class EC2SpotSlave.

--- src/main/java/hudson/plugins/ec2/EC2Computer.java
+++ src/main/java/hudson/plugins/ec2/EC2Computer.java
@@ -21,9 +21,18 @@ public class EC2Computer extends SlaveComputer {
      * Cached description of this EC2 instance. Lazily fetched.
      */
     private volatile Instance ec2InstanceDescription;
+    private String instanceType = "Default Type";
 
     public EC2Computer(EC2Slave slave) {
-        super(slave);
+    	super(slave);
+    	if(slave.getClass().equals(EC2SpotSlave.class))
+        	instanceType = "Spot Slave";
+        else
+        	instanceType = "On Demand Slave";
+    }
+    
+    public String getInstanceType(){
+    	return instanceType;
     }
 
     @Override
