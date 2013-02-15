161b4baf9d7ec540d1062f063a11cb780725a5cd
(B)rian (Wall)ace <nightstrike9809@gmail.com>
2013-02-15 12:46:30 -0500

    "this is why you test" -Adam

--- AMI-Scripts/ubuntu-ami-setup.sh
+++ AMI-Scripts/ubuntu-ami-setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install openjdk-7-jre wget python -y
 
 echo "Downloading boot script"
 sudo wget https://raw.github.com/bwall/ec2-plugin/master/AMI-Scripts/ubuntu-init.py -O /usr/bin/userdata
-sduo chmod +x /usr/bin/userdata
+sudo chmod +x /usr/bin/userdata
 
 echo "Adding boot script to run after boot is complete"
 sudo sed -i 's/exit 0/python \/usr\/bin\/userdata\n&/' /etc/rc.local
