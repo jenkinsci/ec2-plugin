5bf5138c1839b3113897e8abd7d0bf7f7669ffdd
(B)rian (Wall)ace <nightstrike9809@gmail.com>
2013-02-25 21:02:26 -0500

    Powershell Spot scripts

---
+++ AMI-Scripts/powershell-ami-setup.ps1
@@ -0,0 +1,24 @@
+# This script will silently configure a live instance to be able to run as a Jenkins Spot Slave
+# This needs to be run in powershell as Administrator
+# Until we sign these scripts, you will need to run something like the following to allow execution
+# "Set-ExecutionPolicy RemoteSigned"
+
+$downloadLocation = $pwd
+
+# Download Java
+$webclient = New-Object System.Net.WebClient
+$url = "http://javadl.sun.com/webapps/download/AutoDL?BundleId=74788"
+$file = "$downloadLocation\java.exe"
+$webclient.DownloadFile($url,$file)
+
+# Execute Java installer silently
+[System.Diagnostics.Process]::Start("$file", "/s")
+
+# Download Start Up Powershell Script
+$webclient = New-Object System.Net.WebClient
+$url = "https://raw.github.com/bwall/ec2-plugin/master/AMI-Scripts/powershell-init.ps1"
+$file = "$downloadLocation\jenkins-spot-startup.ps1"
+$webclient.DownloadFile($url,$file)
+
+# Run jenkins-spot-startup.ps1 on boot
+[System.Diagnostics.Process]::Start("schtasks.exe", "/create /SC ONSTART /TN JENKINSSPOT /TR 'powershell.exe -noprofile -executionpolicy Unrestricted -file $file'")
