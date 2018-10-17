18814b83cbc866da68d3f0238b37d16ef56c0668
Fabrizio Manfredi Furuholmen <thoulen@users.noreply.github.com>
2018-10-18 01:17:27 +0200

    update test

--- src/test/java/hudson/plugins/ec2/EC2RetentionStrategyTest.java
+++ src/test/java/hudson/plugins/ec2/EC2RetentionStrategyTest.java
@@ -78,6 +78,11 @@ public class EC2RetentionStrategyTest {
             public boolean isOffline() {
                 return false;
             }
+            
+            @Override
+            public InstanceState getState() {
+                return InstanceState.RUNNING;
+            }
         };
         assertTrue(computer.isIdle());
         assertTrue(computer.isOnline());
