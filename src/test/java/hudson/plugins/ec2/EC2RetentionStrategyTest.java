29e71eb314fcbc52d306a318f2e39f90b0ccd936
Danny-Rehelis <Danny_Rehelis@symantec.com>
2018-11-14 15:08:55 +0200

    Added test

--- src/test/java/hudson/plugins/ec2/EC2RetentionStrategyTest.java
+++ src/test/java/hudson/plugins/ec2/EC2RetentionStrategyTest.java
@@ -1,7 +1,7 @@
 package hudson.plugins.ec2;
-
 import com.amazonaws.AmazonClientException;
 import hudson.slaves.NodeProperty;
+import hudson.model.Executor;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -12,13 +12,14 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class EC2RetentionStrategyTest {
 
     @Rule
     public JenkinsRule r = new JenkinsRule();
-
     final AtomicBoolean idleTimeoutCalled = new AtomicBoolean(false);
+    final AtomicBoolean terminateCalled = new AtomicBoolean(false);
 
     @Test
     public void testOnBillingHourRetention() throws Exception {
@@ -35,7 +36,6 @@ public class EC2RetentionStrategyTest {
         expected.add(true);
         upTime.add(new int[] { 60, 00 });
         expected.add(false);
-
         for (int i = 0; i < upTime.size(); i++) {
             int[] t = upTime.get(i);
             EC2Computer computer = computerWithIdleTime(t[0], t[1]);
@@ -51,34 +51,28 @@ public class EC2RetentionStrategyTest {
             @Override
             public void terminate() {
             }
-
             @Override
             public String getEc2Type() {
                 return null;
             }
-
             @Override
             void idleTimeout() {
                 idleTimeoutCalled.set(true);
             }
         };
         EC2Computer computer = new EC2Computer(slave) {
-
             @Override
             public EC2AbstractSlave getNode() {
                 return slave;
             }
-
             @Override
             public long getUptime() throws AmazonClientException, InterruptedException {
                 return ((minutes * 60L) + seconds) * 1000L;
             }
-
             @Override
             public boolean isOffline() {
                 return false;
             }
-            
             @Override
             public InstanceState getState() {
                 return InstanceState.RUNNING;
@@ -86,6 +80,60 @@ public class EC2RetentionStrategyTest {
         };
         assertTrue(computer.isIdle());
         assertTrue(computer.isOnline());
+        return computer;
+    }
+    
+    @Test
+    public void testOnUsageCountRetention() throws Exception {
+        EC2RetentionStrategy rs = new EC2RetentionStrategy("0");
+        List<String> usageCounts = new ArrayList<String>();
+        List<Boolean> expected = new ArrayList<Boolean>();
+        // testing that for usage count of -1 -> terminate shall not be called
+        usageCounts.add("-1");
+        expected.add(false);
+        // testing that for usage count of 0 -> terminate shall be called immediatly
+        usageCounts.add("0");
+        expected.add(true);
+        // testing that for usage count of 1 -> terminate shall be called immediatly
+        usageCounts.add("1");
+        expected.add(true);
+        // testing that for usage count of 2 -> terminate shall not be called
+        usageCounts.add("2");
+        expected.add(false);
+        for (int i = 0; i < usageCounts.size(); i++) {
+            String usageCount = usageCounts.get(i);
+            EC2Computer computer = computerWithUsageLimit(usageCount);
+            Executor executor = new Executor(computer, 0);
+            rs.taskCompleted(executor, null, 0);
+            assertEquals("Expected " + usageCount + " to be " + expected.get(i), (boolean) expected.get(i), terminateCalled.get());
+            // reset the assumption
+            terminateCalled.set(false);
+        }
+        // testing that for usage count of 2 -> terminate shall be called if 2 tasks are completed
+        EC2Computer computer = computerWithUsageLimit("2");
+        Executor executor = new Executor(computer, 0);
+        rs.taskCompleted(executor, null, 0);
+        assertFalse(terminateCalled.get());
+        rs.taskCompleted(executor, null, 0);
+        assertTrue(terminateCalled.get());
+    }
+    private EC2Computer computerWithUsageLimit(final String usageLimit) throws Exception {
+        final EC2AbstractSlave slave = new EC2AbstractSlave("name", "id", "description", "fs", 1, null, "label", null, null, "init", "tmpDir", new ArrayList<NodeProperty<?>>(), "remote", "jvm", false, "idle", null, "cloud", false, false, Integer.MAX_VALUE, null, usageLimit) {
+            @Override
+            public void terminate() {
+                terminateCalled.set(true);
+            }
+            @Override
+            public String getEc2Type() {
+                return null;
+            }
+        };
+        EC2Computer computer = new EC2Computer(slave) {
+            @Override
+            public EC2AbstractSlave getNode() {
+                return slave;
+            }
+        };
         return computer;
     }
 }
