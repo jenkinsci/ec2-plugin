611deb86659eec4ca5b4de31d9933963116f08fa
Raihaan Shouhell <raihaan.shouhell@autodesk.com>
2019-05-21 17:33:37 +0800

    Quote remoteFS on windows

--- src/main/java/hudson/plugins/ec2/win/EC2WindowsLauncher.java
+++ src/main/java/hudson/plugins/ec2/win/EC2WindowsLauncher.java
@@ -74,8 +74,9 @@ public class EC2WindowsLauncher extends EC2ComputerLauncher {
 
             final String jvmopts = node.jvmopts;
             final String remoteFS = node.getRemoteFS();
-            final WindowsProcess process = connection.execute("java " + (jvmopts != null ? jvmopts : "") + " -jar "
-                    + tmpDir + AGENT_JAR + " -workDir " + remoteFS, 86400);
+            final String launchString = "java " + (jvmopts != null ? jvmopts : "") + " -jar " + tmpDir + AGENT_JAR + " -workDir \"" + remoteFS + "\"";
+            logger.println(launchString);
+            final WindowsProcess process = connection.execute(launchString, 86400);
             computer.setChannel(process.getStdout(), process.getStdin(), logger, new Listener() {
                 @Override
                 public void onClosed(Channel channel, IOException cause) {
