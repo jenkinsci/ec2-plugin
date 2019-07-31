892c7d73bcc9b06c77019684babb1eb654c0176b
Raihaan Shouhell <raihaan.shouhell@autodesk.com>
2019-07-31 08:22:07 +0800

    Close remaining streams

--- src/main/java/hudson/plugins/ec2/win/winrm/WindowsProcess.java
+++ src/main/java/hudson/plugins/ec2/win/winrm/WindowsProcess.java
@@ -88,6 +88,9 @@ public class WindowsProcess {
         client.signal();
         client.deleteShell();
         terminated = true;
+        Closeables.closeQuietly(toCallersStdout);
+        Closeables.closeQuietly(toCcallersStdin);
+        Closeables.closeQuietly(toCcallersStderr);
         Closeables.closeQuietly(callersStdout);
         Closeables.closeQuietly(callersStdin);
         Closeables.closeQuietly(callersStderr);
