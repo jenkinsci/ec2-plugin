79f1cea3b4b6b94ad0098f52db0d6964102d7564
Troy Mohl <troymohl@gmail.com>
2020-04-24 14:37:32 -0400

    Add CheckForNull annotation

--- src/main/java/hudson/plugins/ec2/util/AmazonEC2FactoryImpl.java
+++ src/main/java/hudson/plugins/ec2/util/AmazonEC2FactoryImpl.java
@@ -8,11 +8,13 @@ import com.amazonaws.services.ec2.AmazonEC2Client;
 
 import hudson.Extension;
 import hudson.plugins.ec2.EC2Cloud;
+import javax.annotation.CheckForNull;
 
 @Extension
 public class AmazonEC2FactoryImpl implements AmazonEC2Factory {
 
     @Override
+    @CheckForNull
     public AmazonEC2 connect(AWSCredentialsProvider credentialsProvider, URL ec2Endpoint) {
         if (ec2Endpoint != null && ec2Endpoint.toString().trim().length() > 0) {
             AmazonEC2 client = new AmazonEC2Client(credentialsProvider, EC2Cloud.createClientConfiguration(ec2Endpoint.getHost()));
