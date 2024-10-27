e2b61e2c9ac7c5152e62b808356ab51c718a60e6
Zbynek Konecny <zbynek1729@gmail.com>
2024-10-27 13:22:20 +0100

    Use unique ID for forms

--- src/main/resources/hudson/plugins/ec2/EC2Cloud/provision.js
+++ src/main/resources/hudson/plugins/ec2/EC2Cloud/provision.js
@@ -1,6 +1,6 @@
 Behaviour.specify("[data-type='ec2-provision']", 'ec2-provision', -99, function(e) {
   e.addEventListener("click", function (event) {
-    const form = document.querySelector("form[name='provision']");
+    const form = document.getElementById(e.dataset.form);
     form.querySelector("[name='template']").value = e.dataset.url;
     buildFormTree(form);
     form.submit();
