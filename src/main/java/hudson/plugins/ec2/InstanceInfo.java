0a28f735c25c77fb0ab87ce65aadfc7348325beb
mike cirioli <mikecirioli@gmail.com>
2024-10-28 07:30:11 -0400

    fix imports

--- src/main/java/hudson/plugins/ec2/InstanceInfo.java
+++ src/main/java/hudson/plugins/ec2/InstanceInfo.java
@@ -3,8 +3,8 @@ package hudson.plugins.ec2;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.KeyPair;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InstanceInfo {
     private Instance instance;
