f0bf234be2f31d1aaf84c77d3e6d2d0455149255
Basil Crow <me@basilcrow.com>
2025-03-09 14:46:44 -0700

    Partial revert of #1044 (#1055)

--- src/main/java/hudson/plugins/ec2/util/AmazonEC2FactoryImpl.java
+++ src/main/java/hudson/plugins/ec2/util/AmazonEC2FactoryImpl.java
@@ -2,7 +2,7 @@ package hudson.plugins.ec2.util;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.AmazonEC2Client;
 import hudson.Extension;
 import hudson.plugins.ec2.EC2Cloud;
 import java.net.URL;
@@ -12,10 +12,8 @@ public class AmazonEC2FactoryImpl implements AmazonEC2Factory {
 
     @Override
     public AmazonEC2 connect(AWSCredentialsProvider credentialsProvider, URL ec2Endpoint) {
-        AmazonEC2 client = AmazonEC2ClientBuilder.standard()
-                .withCredentials(credentialsProvider)
-                .withClientConfiguration(EC2Cloud.createClientConfiguration(ec2Endpoint.getHost()))
-                .build();
+        AmazonEC2 client =
+                new AmazonEC2Client(credentialsProvider, EC2Cloud.createClientConfiguration(ec2Endpoint.getHost()));
         client.setEndpoint(ec2Endpoint.toString());
         return client;
     }
