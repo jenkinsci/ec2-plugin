38e6da80b412bb3b26d5d44e050adeae44c2d032
ampuscas <apuscas@cloudbees.com>
2025-03-17 13:07:20 +0100

    delete unrelevant tests

--- src/test/java/hudson/plugins/ec2/win/WinConnectionWithFIPSTest.java
+++ src/test/java/hudson/plugins/ec2/win/WinConnectionWithFIPSTest.java
@@ -14,43 +14,6 @@ public class WinConnectionWithFIPSTest {
             FlagRule.systemProperty(FIPS140.class.getName() + ".COMPLIANCE", "true");
 
     /**
-     * Self-signed certificate should not be allowed in FIPS mode, an {@link IllegalArgumentException} is expected
-     */
-    @Test
-    public void testSelfSignedCertificateNotAllowed() {
-        assertThrows(IllegalArgumentException.class, () -> new WinConnection("", "", "", true));
-    }
-
-    /**
-     * It should not be allowed to disable useHTTPS only when a password is used, an {@link IllegalArgumentException} is expected
-     */
-    @Test
-    public void testSetUseHTTPSFalseWithPassword() {
-        assertThrows(IllegalArgumentException.class, () -> new WinConnection("", "alice", "0123456790123456789", false)
-                .setUseHTTPS(false));
-    }
-
-    /**
-     * Password leak prevention when setUseHTTPS is not called
-     */
-    @Test
-    public void testBuildWinRMClientWithoutTLS() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            WinConnection winConnection = new WinConnection("", "alice", "0123456790123456789", false);
-            winConnection.winrm();
-        });
-    }
-
-    /**
-     * When using a password less than 14 chars, an {@link IllegalArgumentException} is expected
-     */
-    @Test
-    public void testSetShortPassword() {
-        assertThrows(
-                IllegalArgumentException.class, () -> new WinConnection("", "alice", "0123", false).setUseHTTPS(false));
-    }
-
-    /**
      * WinConnection class cannot be instantiated in FIPS mode, an {@link IllegalArgumentException} is expected
      */
     @Test
